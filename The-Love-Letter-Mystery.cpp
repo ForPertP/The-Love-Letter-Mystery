6dc5d3a9aafc4f7171c6b5aa1be5bcd1c8764423
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-10-31 01:38:06 +0900

    Update The-Love-Letter-Mystery.cpp

--- The-Love-Letter-Mystery.cpp
+++ The-Love-Letter-Mystery.cpp
@@ -1,1 +1,20 @@
+int theLoveLetterMystery(string s)
+{
+    int result = 0;
+    int i = 0;
+    int j = s.length()-1;
+    
+    while (i <= j)
+    {
+        if (s[i] != s[j])
+        {
+            result += std::abs(s[i]-s[j]);
+        }
+        
+        i++;
+        j--;
+    }
+    
+    return result;
+}
 
