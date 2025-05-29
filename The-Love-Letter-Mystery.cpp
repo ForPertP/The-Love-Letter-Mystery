5311526d22002712184c82b424b799c84c6ad415
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-30 04:21:06 +0900

    Update The-Love-Letter-Mystery.cpp

--- The-Love-Letter-Mystery.cpp
+++ The-Love-Letter-Mystery.cpp
@@ -12,6 +12,20 @@ string rtrim(const string &);
  * The function accepts STRING s as parameter.
  */
 
+int theLoveLetterMystery(const std::string& s)
+{
+    int result = 0;
+    int n = s.length();
+    
+    for (int i = 0; i < n / 2; ++i)
+    {
+        result += std::abs(s[i] - s[n - 1 - i]);
+    }
+    
+    return result;
+}
+
+
 int theLoveLetterMystery(string s)
 {
     int result = 0;
