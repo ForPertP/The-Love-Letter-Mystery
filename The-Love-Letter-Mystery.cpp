80511041faae6bca23840ad43295570e4e00ff3f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-05-31 05:14:43 +0900

    Update The-Love-Letter-Mystery.cpp

--- The-Love-Letter-Mystery.cpp
+++ The-Love-Letter-Mystery.cpp
@@ -63,3 +63,44 @@ int theLoveLetterMystery3(string s)
     return result;
 }
 
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string s;
+        getline(cin, s);
+
+        int result = theLoveLetterMystery(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str)
+{
+    string s(str);
+    s.erase(
+        s.begin(), find_if(s.begin(), s.end(), [](unsigned char ch) { return !isspace(ch); })
+    );
+    return s;
+}
+
+string rtrim(const string &str)
+{
+    string s(str);
+    s.erase(
+        find_if(s.rbegin(), s.rend(), [](unsigned char ch) { return !isspace(ch); }).base(), s.end()
+    );
+    return s;
+}
