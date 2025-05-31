006770eec48e39d12460f46ef4c6ea18dc498053
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-01 05:26:32 +0900

    Update The-Love-Letter-Mystery.cs

--- The-Love-Letter-Mystery.cs
+++ The-Love-Letter-Mystery.cs
@@ -73,10 +73,24 @@ class Result
 }
 
 
-
 class Solution
 {
     public static void Main(string[] args)
     {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            string s = Console.ReadLine();
+
+            int result = Result.theLoveLetterMystery(s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
