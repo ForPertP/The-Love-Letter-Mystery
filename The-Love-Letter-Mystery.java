afab85a43865704090d000c5ff681e9a0f56898c
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2025-06-05 06:34:39 +0900

    Update The-Love-Letter-Mystery.java

--- The-Love-Letter-Mystery.java
+++ The-Love-Letter-Mystery.java
@@ -10,6 +10,56 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
+class Result {
+
+    /*
+     * Complete the 'theLoveLetterMystery' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int theLoveLetterMystery(String s) {
+        int result = 0;
+        int n = s.length();
+
+        for (int i = 0; i < n / 2; ++i) {
+            result += Math.abs(s.charAt(i) - s.charAt(n - 1 - i));
+        }
+
+        return result;
+    }
+
+    public static int theLoveLetterMystery2(String s) {
+        int result = 0;
+        int n = s.length();
+
+        for (int i = 0, j = n - 1; i <= j; ++i, --j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                result += Math.abs(s.charAt(i) - s.charAt(j));
+            }
+        }
+
+        return result;
+    }
+
+    public static int theLoveLetterMystery3(String s) {
+        int result = 0;
+        int i = 0;
+        int j = s.length() - 1;
+
+        while (i <= j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                result += Math.abs(s.charAt(i) - s.charAt(j));
+            }
+
+            i++;
+            j--;
+        }
+
+        return result;
+    }    
+}
 
 
 public class Solution {
